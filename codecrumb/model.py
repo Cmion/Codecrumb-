2c70f4b517c7b67246f02a82e5c7b8d6b549b3c2
Simeon Akpanudo <akpanudosimeon@gmail.com>
2019-07-13 19:33:39 +0100

    default values changes on the crumb and user column

--- codecrumb/model.py
+++ codecrumb/model.py
@@ -24,9 +24,9 @@ class User(db.Model, UserMixin):
     )
     imageUrl = db.Column(db.String(255), nullable=True, unique=True)
     theme = db.Column(db.String(30), default="andromeda", nullable=False, unique=False)
-    f_size = db.Column(db.Integer, default=15, nullable=False, unique=False)
+    f_size = db.Column(db.Integer, default=13, nullable=False, unique=False)
     keymap = db.Column(db.String(30), default="default", nullable=False, unique=False)
-    font = db.Column(db.String(30), default="consolas", nullable=False, unique=False)
+    font = db.Column(db.Text, default="consolas", nullable=False, unique=False)
     snippet = db.Column(db.Text, nullable=True, unique=False)
     dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
     crumbs = db.relationship("Crumbs", backref="author", lazy=True)
@@ -60,8 +60,8 @@ class Crumbs(db.Model):
 class Anonymous_Crumbs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(20), unique=False, nullable=False)
-    crumb_type = db.Column(db.String(20), unique=False, nullable=True, default="normal")
-    crumbName = db.Column(db.Text, nullable=True, unique=False, default="Untitled")
+    crumb_type = db.Column(db.String(255), unique=False, nullable=True, default="normal")
+    crumbName = db.Column(db.Text, nullable=True, unique=False, default="Unnamed Crumb")
     html = db.Column(db.Text, nullable=True, unique=False, default="")
     css = db.Column(db.Text, nullable=True, unique=False, default="")
     js = db.Column(db.Text, nullable=True, unique=False, default="")
@@ -72,10 +72,10 @@ class Anonymous_Crumbs(db.Model):
     cssMode = db.Column(db.Text, nullable=True, unique=False, default="css")
     jsMode = db.Column(db.Text, nullable=True, unique=False, default="javascript")
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    theme = db.Column(db.String(30), default="andromeda", nullable=False, unique=False)
-    f_size = db.Column(db.Integer, default=14, nullable=False, unique=False)
+    theme = db.Column(db.String(50), default="andromeda", nullable=False, unique=False)
+    f_size = db.Column(db.Integer, default=13, nullable=False, unique=False)
     keymap = db.Column(db.String(30), default="default", nullable=False, unique=False)
-    font = db.Column(db.String(30), default="consolas", nullable=False, unique=False)
+    font = db.Column(db.Text, default="consolas", nullable=False, unique=False)
 
     def __repr__(self):
         return f"Crumb(id = {self.id}, User Id = {self.date_created}, Url = {self.url})"
